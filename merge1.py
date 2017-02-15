64a6863e418d18b6890eeefe58ee34308d9515f4
Victor Hugo Diaz <diaz.vhugo@gmail.com>
2017-02-15 01:48:15 -0600

    Update merge1.py

--- merge1.py
+++ merge1.py
@@ -1,8 +1,13 @@
+#!/usr/bin/python
 
 import itertools
 
 a = ['1','2','3']
 b = ['4','5','6','7']
 
-print list(itertools.chain(*zip(a, b)))
-print a+b
+def  merge(a,b):
+	r = list(itertools.chain(*zip(a, b)))
+	return r
+
+r = merge(a,b)
+print r
